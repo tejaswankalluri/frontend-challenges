f80e5df8c8dae42b31dc5bbad16820ab559fa088
tejaswan kalluri <tejaswankalluri82522@gmail.com>
2022-05-26 00:14:59 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    mode: "jit",
+    content: ["*"],
+    theme: {
+        extend: {
+            fontFamily: {
+                manrope: ["Manrope", "sans-serif"],
+            },
+        },
+        colors: {
+            lightcyan: "#cee3e9",
+            neongreen: "#52ffa8",
+            grayishblue: "#4e5d73",
+            darkgrayishblue: "#323a49",
+            darkblue: "#1f2632",
+        },
+    },
+    plugins: [],
+};
