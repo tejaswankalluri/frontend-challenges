2716aa5ce4c78ff8f5903d2b4370b0c46d64f44d
tejaswan kalluri <tejaswankalluri82522@gmail.com>
2022-06-17 19:12:14 +0530

    qr-code done

---
+++ qr-code-component-main/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    mode: "jit",
+    content: ["*"],
+    theme: {
+        extend: {
+            fontFamily: {
+                manrope: ["Manrope", "sans-serif"],
+            },
+        },
+        colors: {
+            white: "#ffff",
+            lightgray: "#d6e2f0",
+            grayishblue: "#7b879d",
+            darkblue: "#1f3251",
+        },
+    },
+    plugins: [],
+};
